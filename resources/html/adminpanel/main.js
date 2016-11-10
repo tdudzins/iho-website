34dc92b53973d5e3d8d51cc00ab83d7798202d85
cjyoung3 <cjyoung3@asu.edu>
2016-11-09 23:17:48 -0700

    Current main.js

--- resources/html/adminpanel/main.js
+++ resources/html/adminpanel/main.js
@@ -5,20 +5,37 @@ function loadEventList() {
         obj.forEach(function(item){
             $("#adaptation-items").append("<li id=\"" + item.eventID + "\" class=\"adpt-unfocused\" >"+ item.eventName + "</li>");
         });
-        setEventListners();
+        setAdptEventListners();
+    })
+    .fail(function(response) {
+        console.log("Failed to load data");
+        window.location = "/error";
+    });
+}
+function setAdptEventListners() {
+    $("li.adpt-unfocused").click(function(){
+        $("li.adpt-focused").removeClass("adpt-focused").addClass("adpt-unfocused");
+        $(this).removeClass("adpt-unfocused").addClass("adpt-focused");
+        updateDiscription($(this).attr("id"));
+        setAdptEventListners();
+    });
+}
+
+function updateDiscription(eID) {
+    $.post("/datafromserver", {action:"q", table:"event", eventid: eID.toFixed(0)}, function(data, status) {
+        var obj = JSON.parse(data);
+
+        $("#adaptation-items").empty();
+        obj.forEach(function(item){
+            $("#adaptation-items").append("<li id=\"" + item.eventID + "\" class=\"adpt-unfocused\" >"+ item.eventName + "</li>");
+        });
+        setAdptEventListners();
     })
     .fail(function(response) {
         console.log("Failed to load data");
         window.location = "/error";
     });
 
-}
-function setEventListners() {
-    $("li.adpt-unfocused").click(function(){
-        $("li.adpt-focused").removeClass("adpt-focused").addClass("adpt-unfocused");
-        $(this).removeClass("adpt-unfocused").addClass("adpt-focused");
-        setEventListners();
-    });
 }
 
 $("#adaptation-items").ready(loadEventList);
