4dff89e796789f795fb5769be97a66a3da05a8e5
cjyoung3 <cjyoung3@asu.edu>
2016-11-09 22:28:49 -0700

    changes

--- resources/html/adminpanel/main.js
+++ resources/html/adminpanel/main.js
@@ -5,15 +5,21 @@ function loadEventList() {
         obj.forEach(function(item){
             $("#adaptation-items").append("<li id=\"" + item.eventID + "\" class=\"adpt-unfocused\" >"+ item.eventName + "</li>");
         });
-        $("li.adpt-unfocused").click(function(){
-            $(this).removeClass("adpt-unfocused").addClass("adpt-focused");
-        });
+        setEventListners();
     })
     .fail(function(response) {
         console.log("Failed to load data");
         window.location = "/error";
     });
 
+}
+function setEventListners() {
+    $("li.adpt-unfocused").click(function(){
+            $(this).removeClass("adpt-unfocused").addClass("adpt-focused");
+            $("li.adpt-focused").click(function(){
+                $(this).removeClass("adpt-focused").addClass("adpt-unfocused");
+            });
+        });
 }
 
 $("#adaptation-items").ready(loadEventList);
