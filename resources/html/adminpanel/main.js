c18752a779b2aaed2a0a3198a94a23dafad3387f
cjyoung3 <cjyoung3@asu.edu>
2016-11-09 22:25:08 -0700

    cjhanges

--- resources/html/adminpanel/main.js
+++ resources/html/adminpanel/main.js
@@ -1,16 +1,19 @@
 function loadEventList() {
     $.post("/datafromserver", {action:"t"}, function(data, status) {
-        console.log(data);
         var obj = JSON.parse(data);
         $("#adaptation-items").empty();
         obj.forEach(function(item){
-            $("#adaptation-items").append("<li id=\"" + item.eventID + ">"+ item.eventName + "</li>");
+            $("#adaptation-items").append("<li id=\"" + item.eventID + "\" class=\"adpt-unfocused\" >"+ item.eventName + "</li>");
         });
-
+        $("li.adpt-unfocused").click(function(){
+            $(this).removeClass("adpt-unfocused").addClass("adpt-focused");
+        });
     })
     .fail(function(response) {
         console.log("Failed to load data");
         window.location = "/error";
     });
+
 }
+
 $("#adaptation-items").ready(loadEventList);
