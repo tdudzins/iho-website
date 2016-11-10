5c62e76c4ad61e1d1d59d41b138a664d9d04c5cb
cjyoung3 <cjyoung3@asu.edu>
2016-11-10 13:15:48 -0700

    code to test

--- resources/html/adminpanel/main.js
+++ resources/html/adminpanel/main.js
@@ -1,3 +1,4 @@
+// gets all the event data and adds it to the list on the left
 function loadEventList() {
     $.post("/datafromserver", {action:"t"}, function(data, status) {
         var obj = JSON.parse(data);
@@ -5,31 +6,23 @@ function loadEventList() {
         obj.forEach(function(item){
             $("#adaptation-items").append("<li id=\"" + item.eventID + "\" class=\"adpt-unfocused\" >"+ item.eventName + "</li>");
         });
-        setAdptEventListners();
+        $("li.adpt-unfocused").click(function(){
+            $("li.adpt-focused").removeClass("adpt-focused").addClass("adpt-unfocused");
+            $(this).removeClass("adpt-unfocused").addClass("adpt-focused");
+            updateDiscription($(this).attr("id"));
+        });
     })
     .fail(function(response) {
         console.log("Failed to load data");
         window.location = "/error";
     });
 }
-function setAdptEventListners() {
-    $("li.adpt-unfocused").click(function(){
-        $("li.adpt-focused").removeClass("adpt-focused").addClass("adpt-unfocused");
-        $(this).removeClass("adpt-unfocused").addClass("adpt-focused");
-        updateDiscription($(this).attr("id"));
-        setAdptEventListners();
-    });
-}
 
 function updateDiscription(eID) {
     $.post("/datafromserver", {action:"q", table:"event", eventid: eID.toFixed(0)}, function(data, status) {
         var obj = JSON.parse(data);
+        console.log(data);
 
-        $("#adaptation-items").empty();
-        obj.forEach(function(item){
-            $("#adaptation-items").append("<li id=\"" + item.eventID + "\" class=\"adpt-unfocused\" >"+ item.eventName + "</li>");
-        });
-        setAdptEventListners();
     })
     .fail(function(response) {
         console.log("Failed to load data");
