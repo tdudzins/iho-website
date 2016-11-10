06da660887d5707afae73f175c669025d560b78f
cjyoung3 <cjyoung3@asu.edu>
2016-11-09 21:39:47 -0700

    loading the list

--- resources/html/adminpanel/main.js
+++ resources/html/adminpanel/main.js
@@ -2,19 +2,15 @@ function loadEventList() {
     $.post("/datafromserver", {action:"t"}, function(data, status) {
         console.log(data);
         var obj = JSON.parse(data);
+        $("#adaptation-items").empty();
+        obj.forEach(function(item){
+            $("#adaptation-items").append("<li id=\"" + item.eventID + ">"+ item.eventName + "</li>");
+        });
+
     })
     .fail(function(response) {
         console.log("Failed to load data");
-        //window.location = "/error";
+        window.location = "/error";
     });
 }
-$.post("/datatoserver",
-{action:"c",
- table:"event", data:["e2","text", 1000000, 100000000, 123, 1234, "reference", "comments", 1]}, function(data, status) {
-    console.log(data);
-})
-.fail(function(response) {
-    console.log("Failed to load data");
-    //window.location = "/error";
-});
 $("#adaptation-items").ready(loadEventList);
