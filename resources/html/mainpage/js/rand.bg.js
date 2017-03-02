d05fba1ba4d1ade9849d24f01f6ffa710db9b1a2
Travis Dudzinski <tdudzins@asu.edu>
2017-03-02 01:24:34 -0700

    Bug fixes/Formatting SideNav Panel/enabling backgroung change

--- resources/html/mainpage/js/rand.bg.js
+++ resources/html/mainpage/js/rand.bg.js
@@ -7,8 +7,8 @@
             count: 10
         }, options);
 
-        //var index = Math.ceil(Math.random() * settings.count * settings.count) % settings.count;
-        var index = 2;
+        var index = Math.ceil(Math.random() * settings.count * settings.count) % settings.count;
+        //var index = 2;
         $(this).addClass(settings.ClassPrefix + index);
     };
 
