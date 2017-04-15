4f9764a5dc3988f25a9cff5d3e17f4a3d7894a7d
Julie Teplik <jteplik@asu.edu>
2017-04-14 22:23:11 -0700

    Finished validation for adminpanel

--- resources/html/adminpanel/main.js
+++ resources/html/adminpanel/main.js
@@ -870,35 +870,40 @@ function disableEditing(id) {
 function dataCheck(id) {
     switch (id) {
         case 'tab-description':
-            if(!$('#adaptationName').val()){
-                alert('You need to add an Adaptation Name.');
+            if(!$('#adaptationName').val()){ 
+                alert('Adaptation Name may not be blank.');
                 return false;
-            }
-            else if(!$('#earliestDirectEvidence').val() && !$('#earliestIndirectEvidence').val()){
-                alert('You need to add Earliest Direct Evidence or Earliest Indirect Evidence.');
+            }else if(!$('#earliestDirectEvidence').val() && !$('#earliestIndirectEvidence').val()){
+				alert('You must provide Earliest Direct Evidence or Earliest Indirect Evidence.');
                 return false;
-            }
-            else if(isNaN(Number( $('#earliestDirectEvidence').val())) || isNaN(Number($('#earliestIndirectEvidence').val()))){
-                alert('Earliest Direct Evidence or Earliest Indirect Evidence must be a number.');
+            }else if(isNaN(Number( $('#earliestDirectEvidence').val()))){
+			    alert('Earliest Direct Evidence must be a number.');
                 return false;
-            }else if($('#earliestDirectEvidence').val() < 0 || $('#earliestIndirectEvidence').val() < 0){
-				alert('Earliest Direct Evidence or Earliest Indirect Evidence must not be less than zero.');
+			}else if(isNaN(Number($('#earliestIndirectEvidence').val()))){
+                alert('Earliest Indirect Evidence must be a number.');
+                return false;
+            }else if($('#earliestDirectEvidence').val() < 0){
+				alert('Earliest Direct Evidence must not be less than zero.');
 				return false;
-			}else if(!$('#earliestDirectEvidence-units').val() == 'Ma' || $('#earliestIndirectEvidence-units').val() == 'Ma'){ //If earliestDirectEvidence or earliestIndirectEvidence are in Ma
-				if($('#earliestDirectEvidence').val() * 1000000 > 8000000 || $('#earliestIndirectEvidence').val() * 1000000 > 8000000){
-					alert('Earliest Direct Evidence or Earliest Indirect Evidence must not be greater than 8 million years.');
-					return false;
-				}
-			}else if($('#earliestDirectEvidence-units').val() == 'Ka' || $('#earliestIndirectEvidence-units').val() == 'Ka'){ //If earliestDirectEvidence or earliestIndirectEvidence are in Ka
-				if($('#earliestDirectEvidence').val() * 1000 > 8000000 || $('#earliestIndirectEvidence').val() * 1000 > 8000000){
-					alert('Earliest Direct Evidence or Earliest Indirect Evidence must not be greater than 8 million years.');
-					return false;
-				}
+			}else if($('#earliestIndirectEvidence').val() < 0){
+				alert('Earliest Indirect Evidence must not be less than zero.');
+				return false;
+			}else if($('#earliestDirectEvidence-units').val() == 'Ma' && $('#earliestDirectEvidence').val() * 1000000 > 8000000){
+				alert('Earliest Direct Evidence must not be greater than 8 million years.');
+				return false;
+			}else if($('#earliestIndirectEvidence-units').val() == 'Ma' && $('#earliestIndirectEvidence').val() * 1000000 > 8000000){
+				alert('Earliest Indirect Evidence must not be greater than 8 million years.');
+				return false;
+			}else if($('#earliestDirectEvidence-units').val() == 'Ka' && $('#earliestDirectEvidence').val() * 1000 > 8000000){
+				alert('Earliest Direct Evidence must not be greater than 8 million years.');
+				return false
+			}else if($('#earliestIndirectEvidence-units').val() == 'Ka' && $('#earliestIndirectEvidence').val() * 1000 > 8000000){
+				alert('Earliest Indirect Evidence must not be greater than 8 million years.');
+				return false;
 			}else if(!$('#ageBoundaryStart').val()){
-                alert('You need to add Age Boundary Start.');
+                alert('Age Boundary Start may not be blank.');
                 return false;
-            }
-            else if(isNaN(Number($('#ageBoundaryStart').val()))){
+            }else if(isNaN(Number($('#ageBoundaryStart').val()))){
                 alert('Age Boundary Start must be a number.');
                 return false;
             }else if($('#ageBoundaryStart').val() < 0){
@@ -911,7 +916,7 @@ function dataCheck(id) {
 				alert('Age Boounary Start must be less than 8 million years');
 				return false;
 			}else if(!$('#ageBoundaryEnd').val()){
-                alert('You need to add Age Boundary End.');
+                alert('Age Boundary End may not be blank.');
                 return false;
             }else if(isNaN(Number($('#ageBoundaryEnd').val()))){
                 alert('Age Boundary End must be a number.');
@@ -925,8 +930,9 @@ function dataCheck(id) {
 			}else if($('#ageBoundaryEnd-units').val() == 'Ka' && $('#ageBoundaryEnd').val() * 1000 > 8000000){
 				alert('Age Boundary End must be less than 8 million years.');
 				return false;
-			}else
+			}else{
                 return true;
+			}
             break;
         case 'tab-media':
             if(!$('#mediaDescription').val()){
